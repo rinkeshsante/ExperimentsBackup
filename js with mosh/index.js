39e100e5abc757531878fc4647a7edbc1a2d5c6d
RinkeshSante <52735633+rinkeshsante@users.noreply.github.com>
2020-05-20 11:35:20 +0530

    added more side projects

---
+++ js with mosh/index.js
@@ -0,0 +1,13 @@
+console.log("hey there")
+
+
+// let to declare variable 
+let name ="ok"
+name = 89
+console.log(name)
+
+// const to declare constant
+const name1 =10.900000
+console.log(name1)
+
+//
